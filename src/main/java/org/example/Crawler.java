0057d7d2e69452052d9212005fff5098468d24f6
Param-Veer-Singh <122340319+Param-Veer-Singh@users.noreply.github.com>
2023-05-03 10:51:57 +0530

    Add files via upload

--- src/main/java/org/example/Crawler.java
+++ src/main/java/org/example/Crawler.java
@@ -1,7 +1,44 @@
 package org.example;
 
-public class Main {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.HashSet;
+
+public class Crawler {
+
+    HashSet<String> urlSet;
+    int maxDepth = 2;
+    Crawler(){
+        urlSet = new HashSet<>();
+    }
+    public void getPageTextAndLinks(String url, int depth){
+        if(urlSet.contains(url))return;
+        if(depth > maxDepth)return;
+        urlSet.add(url);
+        depth++;
+        try{
+            Document document = Jsoup.connect(url).timeout(5000).get();
+            Indexer indexer = new Indexer(document, url);
+            System.out.println(document.title());
+            Elements avlLinkOnPage = document.select("a[href]");
+            for(Element currentLink : avlLinkOnPage){
+                getPageTextAndLinks(currentLink.attr("abs:href"), depth);
+            }
+        }
+        catch (IOException ioException){
+            ioException.printStackTrace();
+        }
+
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Crawler crawler = new Crawler();
+        //crawler.getPageTextAndLinks("https://javatpoint.com",1);
+        crawler.getPageTextAndLinks("https://practice.geeksforgeeks.org",1);
+        crawler.getPageTextAndLinks("https://leetcode.com/problemset/all",1);
     }
 }
--- src/main/java/org/example/DatabaseConnection.java
+++ src/main/java/org/example/DatabaseConnection.java
@@ -1,2 +1,30 @@
-package org.example;public class DatabaseConnection {
+package org.example;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverAction;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    static Connection connection = null;
+
+    public static Connection getConnection() {
+        if(connection != null) return connection;
+        String user = "root";
+        String pwd = "1234";
+        String db = "searchengine";
+        return getConnection(user,pwd,db);
+    }
+    private static Connection getConnection(String user, String pwd, String db)  {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/" + db + "?user=" + user + "&password=" +  pwd);
+        }
+        catch (SQLException | ClassNotFoundException sqlException){
+            sqlException.printStackTrace();
+        }
+        return connection;
+    }
 }
